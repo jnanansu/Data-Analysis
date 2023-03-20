b8351f21e7fa53d38faaee90d0b9e2434bfef820
jnanansu prakash bisoi <jnanansub@gmail.com>
2023-03-20 17:36:44 +0530

    Adding all operations

--- Python_Code1.py
+++ Python_Code1.py
@@ -1,6 +1,14 @@
-def first_funtion(A,B):
-    return A+B
+# Function
+def operation(A,B, op):
+    if op == "sum":
+        return A+B
+    if op == "prod":
+        return A*B
+    if op == "minus":
+        return A - B
+    if op == "div":
+        return A/B
 
+# Define Main function
 if __name__ == '__main__':
-    sum = first_funtion(2,5)
-    print(sum)
+    print(operation(2,5,"div"))
